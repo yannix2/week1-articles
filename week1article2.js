5a81150e8482bd2b1ce1e73a5632abdf4fa6fec3
yassine khiari <tasssannas@gmail.com>
2021-06-30 02:57:56 +0100

    article's tasks for week1

---
+++ week1article2.js
@@ -0,0 +1,23 @@
+let browser = 'raednetwork'
+if (browser == 'edge')
+{
+  console.log('You have got the Edge!<')
+}
+else if (browser == 'chrome' || browser== 'mozila' || browser=='opera' ||browser=='safari')
+ { console.log('Okay we support these browsers too')}
+ else { console.log('We hope that this page looks ok!' )}
+ 
+ 
+ //TASK1//
+
+let a=8
+switch(a){
+case 0 : console.log(0)
+break ;
+case 1 : console.log(1)
+break ;
+case 2||3  : console.log('2,3')
+break ;
+default : console.log('erreur')
+}
+// TASK2 // 
